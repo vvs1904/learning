0982eada6acded8eac5a6157e3cd7a1c205b981a
Vanshika <vanshikasinha886@gmail.com>
2026-05-26 13:04:47 +0530

    Added fundamentals-1 folder

--- fundamentals 1/assign1.js
+++ fundamentals 1/assign1.js
@@ -1,6 +1,7 @@
 let country = "India";
 let continent = "Asia";
 let population = 135;
+console.log(`I love my ${country} with ${population}.`);
 console.log(country);
 console.log(continent);
 console.log(population);
